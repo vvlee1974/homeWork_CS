eeb2d6618e5a7dccafaa052638133b8a664e217b
vvlee <vvlee@gmail.com>
2022-12-17 18:56:48 +0500

    add change in Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -23,7 +23,7 @@ Console.WriteLine("Введите первое число: ");
 int a = int.Parse(Console.ReadLine() ?? "0");
 
 Console.WriteLine("Введите второе число: ");
-int b = int.Parse(Console.ReadLine() ?? "0" ?? "string");
+int b = int.Parse(Console.ReadLine() ?? "0");
 
 if(a > b)
 {
